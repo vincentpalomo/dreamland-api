1413b3f4abdca88093910065b4b33812cd55cf7b
vincent <vincentpalomo@yahoo.com>
2023-01-02 21:47:54 -0600

    added more characters to the database

--- controllers/data.js
+++ controllers/data.js
@@ -5,6 +5,8 @@ const data = [
     age: 19,
     gender: 'Female',
     race: 'Human',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/8/85/Queen_Tiabeanie_of_Dreamland.png/revision/latest/scale-to-width-down/350?cb=20220122065631',
   },
   {
     id: 2,
@@ -12,6 +14,8 @@ const data = [
     age: 18,
     gender: 'Male',
     race: '50% Elf 50% Ogre',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/0/0e/Elfo.jpg/revision/latest/scale-to-width-down/294?cb=20180523182247',
   },
   {
     id: 3,
@@ -19,6 +23,8 @@ const data = [
     age: 300,
     gender: 'Male',
     race: 'Sub Demon',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/8/8d/Lucille.png/revision/latest/scale-to-width-down/343?cb=20200523051336',
   },
   {
     id: 4,
@@ -26,6 +32,8 @@ const data = [
     age: 55,
     gender: 'Male',
     race: 'Human',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/5/50/Zog.jpg/revision/latest/scale-to-width-down/350?cb=20180817235827',
   },
   {
     id: 5,
@@ -33,6 +41,86 @@ const data = [
     age: 40,
     gender: 'Female',
     race: 'Human',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/6/6d/Dagmar_of_Hell.jpg/revision/latest/scale-to-width-down/350?cb=20220226143418',
+  },
+  {
+    id: 6,
+    character: 'Queen Oona',
+    age: 40,
+    gender: 'Female',
+    race: 'Salamander',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/e/ef/Pirate_Queen_Oona.jpg/revision/latest/scale-to-width-down/329?cb=20210216195406',
+  },
+  {
+    id: 7,
+    character: 'Jasper',
+    age: 13,
+    gender: 'Male',
+    race: '50% Selkie 50% Human',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/9/9c/Bear_boy.jpg/revision/latest/scale-to-width-down/350?cb=20220212132722',
+  },
+  {
+    id: 8,
+    character: 'Prince Derek',
+    age: 13,
+    gender: 'Male',
+    race: '50% Salamander 50% Human',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/2/26/PDerek.jpg/revision/latest/scale-to-width-down/350?cb=20220212200204',
+  },
+  {
+    id: 9,
+    character: 'Ursula',
+    age: 40,
+    gender: 'Female',
+    race: 'Selkie',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/0/0e/Ursulaa.jpg/revision/latest/scale-to-width-down/350?cb=20191008104754',
+  },
+  {
+    id: 10,
+    character: 'Arch-Druidess',
+    age: 45,
+    gender: 'Female',
+    race: 'Human',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/8/83/Priestess_front.jpg/revision/latest/scale-to-width-down/239?cb=20181002214554',
+  },
+  {
+    id: 11,
+    character: 'Odval',
+    age: 60,
+    gender: 'Male',
+    race: 'Unknown Humanoid',
+    image:
+      'https://static.wikia.nocookie.net/disenchantment/images/b/b2/Odval.jpg/revision/latest/scale-to-width-down/346?cb=20180818043416',
+  },
+  {
+    id: 11,
+    character: '',
+    age: 13,
+    gender: '',
+    race: '',
+    image: '',
+  },
+  {
+    id: 11,
+    character: '',
+    age: 13,
+    gender: '',
+    race: '',
+    image: '',
+  },
+  {
+    id: 11,
+    character: '',
+    age: 13,
+    gender: '',
+    race: '',
+    image: '',
   },
 ];
 
